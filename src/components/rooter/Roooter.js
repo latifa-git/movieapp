aff58e256b9a5de47440d5d9b6ea12ee0a855804
latifa-git <latifabenameur96@gmail.com>
2021-06-03 15:40:08 +0100

    5commit

--- src/components/rooter/Roooter.js
+++ src/components/rooter/Roooter.js
@@ -2,13 +2,14 @@ import React from "react";
 import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
+
 const Roooter = ({filmlist, match}) => {
   const film = filmlist.find(el => el.Id.toString() === match.params.Id.toString()
   );
   return (
     <div>
       <h2> welcome</h2>
-      <h2> {film.ImgSRC}</h2>
+      <h2> {film.Descption}</h2>
    
       <Link to={"/"}>
         <Button variant="primary">Go Back</Button>
